242b1fbb1d7e8fb11b38676b440b31b55fb3dcbd
unknown <pggian1985@gmail.com>
2024-08-03 10:54:32 -0300

    add: configurado conexao com banco, adicionado models de teste

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
-const app = express();
+const { sequelize, connectToDatabase } = require('./config/database'); 
+const models = require('./models');
 
-app.listen(()=>{
-    console.log('serve up!')
-})
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+(async () => {
+    try {
+        await connectToDatabase();  
+        
+        await sequelize.sync(); 
+        
+        app.listen(PORT, () => {
+            console.log(`Servidor conectado na porta ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
---
+++ src/models/index.js
@@ -0,0 +1,5 @@
+const User = require('./user.model');
+const Item = require('./item.model');
+
+
+module.exports = {User,Item}
