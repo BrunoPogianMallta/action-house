4eaaacc5170ff2596218761c17560f982328cd0e
unknown <pggian1985@gmail.com>
2024-08-06 20:18:58 -0300

    add: cors da vercel

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ const express = require('express');
 const { sequelize, connectToDatabase } = require('./config/database'); 
 const routes = require('./routes');
 const cors = require('cors');
+require('dotenv').config(); // Certifique-se de que as variáveis de ambiente estão carregadas
 
 const app = express();
-app.use(cors());
-app.use(express.json()); // Middleware para parsing do corpo da requisição
 
-// Middleware para parsing de dados de formulário (opcional, caso esteja usando formulários)
-app.use(express.urlencoded({ extended: true }));
+// Configurar CORS para permitir solicitações do frontend
+const corsOptions = {
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000', // URL do frontend, altere conforme necessário
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
+app.use(express.json()); // Middleware para parsing do corpo da requisição
+app.use(express.urlencoded({ extended: true })); // Middleware para parsing de dados de formulário
 
 app.use('/api/v1', routes);
 
@@ -17,7 +24,6 @@ const PORT = process.env.PORT || 3000;
 (async () => {
     try {
         await connectToDatabase();  
-        
         await sequelize.sync(); 
         
         app.listen(PORT, () => {
