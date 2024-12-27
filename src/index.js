046586faaa046ddb8071cd1a5d5d850718072bdd
unknown <pggian1985@gmail.com>
2024-12-27 06:02:36 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -7,33 +7,35 @@ require('dotenv').config();
 
 const app = express();
 
-
+// Configurações de CORS
 const corsOptions = {
-    origin: [process.env.LOCAL_FRONTEND_URL, process.env.FRONTEND_URL, 'localhost'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
+    origin: [process.env.LOCAL_FRONTEND_URL, process.env.FRONTEND_URL], // URLs permitidas para acessar o backend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    credentials: true // Permitir envio de cookies e headers de autenticação
 };
 
-app.use(cors(corsOptions));
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true }));
+// Middlewares
+app.use(cors(corsOptions)); // Configuração de CORS
+app.use(express.json()); // Suporte para JSON no body
+app.use(express.urlencoded({ extended: true })); // Suporte para formulários no body
 
-
+// Rotas
 app.use('/api/v1', routes);
 
-
+// Porta do servidor
 const PORT = process.env.PORT || 3000;
 
-
+// Inicialização do servidor
 (async () => {
     try {
-        
+        // Conexão com o banco de dados
         await connectToDatabase();  
         await sequelize.sync(); 
         
-        
+        // Inicia o servidor
         app.listen(PORT, () => {
             console.log(`Servidor rodando na porta ${PORT}`);
+            console.log(`Permitindo conexões de: ${process.env.LOCAL_FRONTEND_URL}, ${process.env.FRONTEND_URL}`);
         });
     } catch (error) {
         console.error('Não foi possível conectar ao banco de dados:', error);
