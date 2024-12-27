c60a8ab8f8c0887381d0d2cafad2abac48c0c001
unknown <pggian1985@gmail.com>
2024-12-27 06:31:22 -0300

    commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,33 +1,22 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const isProduction = process.env.APP_ENV === 'production';
-
-
+// Conexão ao banco de dados local
 const sequelize = new Sequelize(
-    isProduction
-        ? process.env.DATABASE_URL
-        : `postgres://${process.env.LOCAL_DB_USER}:${process.env.LOCAL_DB_PASSWORD}@${process.env.LOCAL_DB_HOST}:${process.env.LOCAL_DB_PORT}/${process.env.LOCAL_DB_NAME}`,
+    `postgres://${process.env.LOCAL_DB_USER}:${process.env.LOCAL_DB_PASSWORD}@${process.env.LOCAL_DB_HOST}:${process.env.LOCAL_DB_PORT}/${process.env.LOCAL_DB_NAME}`,
     {
         dialect: 'postgres',
-        dialectOptions: isProduction
-            ? {
-                ssl: {
-                    require: true,
-                    rejectUnauthorized: false, // Necessário para servidores sem certificados confiáveis
-                },
-            }
-            : {},
-        logging: false,
+        dialectOptions: {},
+        logging: false,  // Desativa o log de queries
     }
 );
 
 const connectToDatabase = async () => {
     try {
-        await sequelize.authenticate();
-        console.log('Conexão com o banco de dados estabelecida com sucesso.');
+        await sequelize.authenticate();  // Tenta autenticar com o banco de dados local
+        console.log('Conexão com o banco de dados local estabelecida com sucesso.');
     } catch (error) {
-        console.error('Não foi possível conectar ao banco de dados:', error);
+        console.error('Não foi possível conectar ao banco de dados local:', error);
     }
 };
 
