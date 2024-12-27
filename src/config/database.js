c78ec68ca2fce0115306a0a21c7c03f0c97ec494
unknown <pggian1985@gmail.com>
2024-12-27 06:38:55 -0300

    commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,22 +1,24 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-// Conexão ao banco de dados local
-const sequelize = new Sequelize(
-    `postgres://${process.env.LOCAL_DB_USER}:${process.env.LOCAL_DB_PASSWORD}@${process.env.LOCAL_DB_HOST}:${process.env.LOCAL_DB_PORT}/${process.env.LOCAL_DB_NAME}`,
-    {
-        dialect: 'postgres',
-        dialectOptions: {},
-        logging: false,  // Desativa o log de queries
-    }
-);
+// Conexão ao banco de dados remoto usando a variável DATABASE_URL
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // Necessário para servidores sem certificados confiáveis
+        },
+    },
+    logging: false,  // Desativa o log de queries
+});
 
 const connectToDatabase = async () => {
     try {
-        await sequelize.authenticate();  // Tenta autenticar com o banco de dados local
-        console.log('Conexão com o banco de dados local estabelecida com sucesso.');
+        await sequelize.authenticate();  // Tenta autenticar com o banco de dados remoto
+        console.log('Conexão com o banco de dados remoto estabelecida com sucesso.');
     } catch (error) {
-        console.error('Não foi possível conectar ao banco de dados local:', error);
+        console.error('Não foi possível conectar ao banco de dados remoto:', error);
     }
 };
 
